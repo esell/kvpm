b24bb488b4a4f1d32df5b5c06ade0f6e27deacea
esell <esell@esheavyindustries.com>
2018-07-16 20:56:59 -0600

    uh, that shouldn't be there...

--- main.go
+++ main.go
@@ -127,8 +127,7 @@ func getSecret(basicClient keyvault.BaseClient, secname string) {
 		fmt.Printf("unable to get value for secret: %v\n", err)
 		os.Exit(1)
 	}
-	//	fmt.Println(*secretResp.Value)
-	fmt.Printf("%v\n", secretResp)
+	fmt.Println(*secretResp.Value)
 }
 
 func createUpdateSecret(basicClient keyvault.BaseClient, secname, secvalue string) {
